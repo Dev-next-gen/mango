991ce1a81ae8744faf29a341f7776d4f3b583223
leoca <leo.camus23@gmail.com>
2026-09-20 14:23:33 +0000

    fix: use preprocessor defines for client prefix instead of static variables
    Signed-off-by: leoca <leo.camus23@gmail.com>

--- src/ipc/ipc.c
+++ src/ipc/ipc.c
@@ -828,8 +828,8 @@ void handle_command(int client_fd, const char *cmd_raw) {
 	} else if (strcmp(cmd, "get layouts") == 0) {
 		resp = build_layouts_response();
 	} else if (strncmp(cmd, "dispatch ", 9) == 0) {
-		static const char client_prefix[] = "client,";
-		static const int client_prefix_len = sizeof(client_prefix) - 1;
+#define CLIENT_PREFIX "client,"
+#define CLIENT_PREFIX_LEN (sizeof(CLIENT_PREFIX) - 1)
 		char *dispatch_copy = strdup(cmd_raw + 9);
 		char *out = dispatch_copy, *ptr = dispatch_copy;
 		int client_id = -1;
@@ -846,10 +846,10 @@ void handle_command(int client_fd, const char *cmd_raw) {
 			// "client,<id>" must start a field, otherwise it would match the
 			// tail of a name like "viewtoleft_have_client,1".
 			if (field_start &&
-				strncmp(ptr, client_prefix, client_prefix_len) == 0) {
+				strncmp(ptr, CLIENT_PREFIX, CLIENT_PREFIX_LEN) == 0) {
 				char *end;
-				long id = strtol(ptr + client_prefix_len, &end, 10);
-				if (id > 0 && end > ptr + client_prefix_len &&
+				long id = strtol(ptr + CLIENT_PREFIX_LEN, &end, 10);
+				if (id > 0 && end > ptr + CLIENT_PREFIX_LEN &&
 					(*end == '\0' || *end == ',')) {
 					client_id = (int)id;
 					ptr = end;
